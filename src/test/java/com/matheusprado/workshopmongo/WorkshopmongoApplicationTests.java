9c64eeed8e4d5f51608acc8807261313b43730bd
MatheusPMC <matheus.pmc@hotmail.com>
2021-02-27 20:36:09 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/matheusprado/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusprado.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
